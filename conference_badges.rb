556be863239c607da9d1d63ccc7e807cec37cddc
Carrie Mathieu <carrieburks@gmail.com>
2020-08-09 03:23:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(names)
+  puts batch_badge_creator(attendees)
   assign_rooms(attendees).each do |room|
     puts room
   end
