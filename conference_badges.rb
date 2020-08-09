dce3f20fe4bdf513c8fede03a48e516658a8547d
Carrie Mathieu <carrieburks@gmail.com>
2020-08-09 02:59:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge_messages = []
+  names.each do |name|
+    badge_messages << "Hello, my name is #{name}."
+  return badge_messages
+end
+
+#assign_rooms(speakers_list)
+#assugns each speaker to rooms 1-7 (one per room)
+#returns "Hello, #{name}! You'll be assigned to room #{room}"
+#each_with_index - access array by index number
+# return value of each is original array, will need to store assignment strings
+#end
+
+#def printer
+# output results of batch_badge_creater
+# assign_rooms 
+  #will need to iterate over array and "puts" out each
+#end
