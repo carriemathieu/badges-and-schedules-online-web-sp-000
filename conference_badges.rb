a6383903e4ddc931734d69d29cc914450954dff5
Carrie Mathieu <carrieburks@gmail.com>
2020-08-09 03:17:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,12 +20,6 @@ def assign_rooms(attendees)
   end
   return room_assignments
 end
-    
-#assugns each speaker to rooms 1-7 (one per room)
-#returns "Hello, #{name}! You'll be assigned to room #{room}"
-#each_with_index - access array by index number
-# return value of each is original array, will need to store assignment strings
-#end
 
 #def printer
 # output results of batch_badge_creater
